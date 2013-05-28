a2e1339f59fabd79a911a98d93dde090af19105e
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 12:54:28 -0400

    current path

--- recipes/rails-restart.rb
+++ recipes/rails-restart.rb
@@ -16,7 +16,7 @@ node[:deploy].each do |application, deploy|
 	Chef::Log.info("Precompiling assets for RAILS_ENV=#{rails_env}...")
 	
 	execute "rake assets:precompile" do
-		cwd release_path
+		cwd deploy[:current_path]
 		command "bundle exec rake assets:precompile"
 		environment "RAILS_ENV" => rails_env
 	end
